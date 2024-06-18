7fc92a175af63dc8dccaa2cebc2827c9003ff0f2
Cortezano <christianjakecortezano@gmail.com>
2024-06-18 22:01:27 +0800

    first commit

---
+++ chatroom.lua
@@ -0,0 +1,32 @@
+Members = Members or {}
+
+Handlers.add(
+    "Register",
+    Handlers.utils.hasMatchingTag("Action", "Register"),
+    function(msg)
+        table.insert(Members, msg.From)
+        Handlers.utils.reply("registered")(msg) -- this reply will show if the user is already registered!!
+    end
+)
+-- this handler will allow processes to register to the chatroom by responding to the tag
+-- Action = "Register". A printed message will confirm stating registered wil appear when the registration is Succesfull
+
+Handlers.add(
+    "Broadcast",
+    Handlers.utils.hasMatchingTag("Action", "Broadcast"),
+    function(msg)
+        for _, recipient in ipairs(Members) do
+            ao.send({ Target = recipient, Data = msg.Data })
+        end
+        Handlers.utils.reply("Broadcasted.")(msg) --this reply show if this chatroom is broadcast
+    end
+)
+--This handler will allow you to broadcast messages to all members of the chatroom.
+
+-- Trinity process ID = TLP_5xtNWzDAU_V565avSyP98X2wClrVs0QODOghagU
+-- after this I will invite trinity to join in chatroom
+
+-- the next challenge if create a token, well be using it to tokengate this chatroom. Do that, then you will impressed them
+-- their two things to build token,
+-- the blueprint - this is a predesigned template that helps you quickly build a token in ao
+-- the manual method - this is step by step guide to building a token in ao from scratch
---
+++ main.lua
@@ -0,0 +1,14 @@
+print("I am cool!");
+-- aos then will show the aos operating system
+-- creating variable called Morpheus
+-- then give this value the process Id
+
+-- Check the Morpheus variable by typing `Morpheus`
+-- Morpheus
+-- Expected Results:
+-- ajrGnUq9x9-K1TY1MSiKwNWhNTbq7-IdtFa33T59b7s
+
+-- If `undefined` is returned,
+-- then the variable was not created successfully.
+
+-- to send a message use "Send({ Target = "To send a message to a specific process", Data = "your message here!"})"
